e4fbbe8428fa33e9d5d2cf5941214523d30f9b21
Bistowsky <dafa.130.df@gmail.com>
2024-04-05 12:06:11 +0700

    broken project wkwkw

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
 import streamlit as st
-
+import os
 
 load_dotenv()
+KEY=os.getenv("OPENAI_API_KEY")
+
+def get_openai_response(question):
+    llm=OpenAI(
+        openai_api_key=KEY, 
+        model_name="gpt-3.5-turbo",
+        temperature=0.5
+    )
+    response=llm(question)
+    return response
+
+st.set_page_config(page_title="Q&A Chatbot")
+
+st.header("langchain chatbot")
+
+input=st.text_input("Input", key='input')
+response=get_openai_response(input)
+
+submit=st.button("Ask the question")
+
+if submit:
+    st.subheader("the response is")
+    st.write(response)
 
